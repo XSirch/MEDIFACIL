3501f0b36b78bcf24e000169210ee3ef0a029dda
XSirch <christianoccruz@yahoo.com.br>
2024-05-04 19:59:03 -0300

    static

--- healing/urls.py
+++ healing/urls.py
@@ -26,5 +26,5 @@ urlpatterns =[
     path('medicos/', include('medicos.urls')),
     path('pacientes/', include('pacientes.urls')),
     path('', lambda request: redirect('pacientes/home'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
